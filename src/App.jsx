44aed42243089410fd2d79ee42582ddd28db0891
TiredBirb <72065138+anna636@users.noreply.github.com>
2022-02-01 09:50:20 +0100

    Asses sign in and out

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { PageLayout } from "./components/PageLayout"
+import {
+  AuthenticatedTemplate,
+  UnauthenticatedTemplate,
+} from "@azure/msal-react";
+
 
 function App() {
   return (
-    <div className="App">
-     <p>Msal test here</p>
-    </div>
+    <PageLayout>
+      <AuthenticatedTemplate>
+        <p>You are signed in!</p>
+      </AuthenticatedTemplate>
+      <UnauthenticatedTemplate>
+        <p>You are not signed in! Please sign in.</p>
+      </UnauthenticatedTemplate>
+    </PageLayout>
   );
 }
 
---
+++ src/authConfig.js
@@ -0,0 +1,15 @@
+export const msalConfig = {
+  auth: {
+    clientId: "7a8aa7f3-752b-494f-9480-0c5cd5ecabc9",
+    authority:
+      "https://login.microsoftonline.com/cd20e4c9-f82c-4d3e-9224-90f2bc4be1a0",
+    redirectUri: "/",
+  },
+  cache: {
+    cacheLocation: "localStorage",
+  },
+};
+
+export const loginRequest = {
+  scopes: ["User.Read"],
+};
